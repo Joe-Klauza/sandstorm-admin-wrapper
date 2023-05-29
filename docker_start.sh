4445143900b97812e2bbed417a2e15f560acd524
leiflundgren <leiflundgren@users.noreply.github.com>
2023-05-30 00:02:40 +0200

    Newer steamcmd requires force_install_dir before login command (#137)
    * fixes #1  Newer steamcmd requires force_install_dir before login command
    Changed the order of the commands
    
    * fixes #1 Newer steamcmd requires force_install_dir before login command
    - update docker_start.sh too

--- docker_start.sh
+++ docker_start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Install the server if not already installed
+# Install the server if not already installed 
 if [[ ! -d sandstorm-server/Insurgency ]]
 then
-  steamcmd/installation/steamcmd.sh +login anonymous +force_install_dir /home/sandstorm/sandstorm-server +app_update 581330 +quit
+  steamcmd/installation/steamcmd.sh +force_install_dir +login anonymous /home/sandstorm/sandstorm-server +app_update 581330 +quit
 fi
 
 # Start normally
